6d88e38fd77ddae3bb8b2db26e586c1891ef16ad
Somacal2 <110057784+Somacal2@users.noreply.github.com>
2022-10-29 18:21:51 -0300

    First Commit

---
+++ EditorHtml/Abrir.cs
@@ -0,0 +1,23 @@
+
+namespace EditorHtml
+{
+    public class Abrir
+    {
+        public static void Show()
+        {
+            Console.Clear();
+            Console.WriteLine("Qual o caminho do arquivo?");
+            string path = Console.ReadLine();
+
+            using (var file = new StreamReader(path))
+            {
+                string text = file.ReadToEnd();
+                Console.WriteLine(text);
+            }
+
+            Console.WriteLine("");
+            Console.ReadLine();
+            Menu.Show();
+        }
+    }
+}
